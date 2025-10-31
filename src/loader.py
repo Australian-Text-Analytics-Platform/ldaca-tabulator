426e7eb13840f847b3a1bdc51f694311d565d818
Atiq Ur Rehman <atiqur.rehman@sydney.edu.au>
2025-10-31 13:41:48 +1100

    class name

--- src/loader.py
+++ src/loader.py
@@ -1,3 +1,25 @@
+import os
+import zipfile
+import requests 
+from io import BytesIO
+from rocrate_tabular.tabulator import ROCrateTabulator
+
 
 class loader:
-    pass
+    database = 'corpus.db'
+    folder = 'corpus'
+    def __init__(self, corpus):
+        self.corpus = corpus
+
+    def fetch_zip_folder(self):
+        cwd = os.getcwd()
+        extract_to = os.path.join(cwd, folder)
+        os.makedirs(extract_to, exist_ok=True)
+        response = requests.get(self.corpus, stream=True)
+        response.raise_for_status()
+        with zipfile.ZipFile(BytesIO(response.content)) as zip_ref:
+            zip_ref.extractall(extract_to)
+    
+    def fetch_data(sellf):
+        pass
+
