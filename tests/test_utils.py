3e863ce5622ba93548f2005d5e1bff79c626e56b
Atiq Ur Rehman <atiqur.rehman@sydney.edu.au>
2025-11-17 13:34:04 +1100

    Adding test for unzip_corpus function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,19 @@
-from src.utils import load_config
+from src.utils import (load_config,
+                       load_table_from_db,
+                       unzip_corpus)
+#import sqlite3
+from rocrate_tabular.tabulator import ROCrateTabulator
 
-def test_load_config():
-    config_file = load_config("./tests/crates/minimal/ro-crate-metadata.json")
+def test_load_config(path = "./tests/crates/minimal/ro-crate-metadata.json"):
+    config_file = load_config(path)
     assert '@graph' in config_file
+
+def test_unzip_corpus(url = "./tests/crates/languageFamily.zip", tb = ROCrateTabulator()):
+    unzip_corpus(url, tb)
+    pass
+
+def test_load_table_from_db():
+    pass
+
+
 
