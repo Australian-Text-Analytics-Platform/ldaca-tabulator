8f9d3f61536647486add80bb18b947d3e5c2db97
Atiq Ur Rehman <atiqur.rehman@sydney.edu.au>
2025-11-17 17:24:35 +1100

    adding test functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,11 @@
-from src.utils import (load_config,
-                       load_table_from_db,
-                       unzip_corpus)
-#import sqlite3
-from rocrate_tabular.tabulator import ROCrateTabulator
+from src.utils import (load_config)
+
 
 def test_load_config(path = "./tests/crates/minimal/ro-crate-metadata.json"):
     config_file = load_config(path)
     assert '@graph' in config_file
 
-def test_unzip_corpus(url = "./tests/crates/languageFamily.zip", tb = ROCrateTabulator()):
-    unzip_corpus(url, tb)
-    pass
-
 def test_load_table_from_db():
     pass
-
 
 
