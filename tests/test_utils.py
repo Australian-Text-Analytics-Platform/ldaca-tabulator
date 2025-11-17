cd98743377ee6f4efb662d3bd577ce3ce45a0908
Atiq Ur Rehman <atiqur.rehman@sydney.edu.au>
2025-11-17 12:53:58 +1100

    coorecting test for load_config function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 from src.utils import load_config
 
 def test_load_config():
-    config_file = load_config("./crates/minimal/re-crate-metadata.json")
+    config_file = load_config("./tests/crates/minimal/ro-crate-metadata.json")
     assert '@graph' in config_file
+
