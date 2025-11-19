e1e654c54748e42a1fd86ecd8db7f400413a2cec
Atiq Ur Rehman <atiqur.rehman@sydney.edu.au>
2025-11-19 13:26:23 +1100

    Cleaing LDaCATabulator class

--- src/tabulator.py
+++ src/tabulator.py
@@ -56,15 +56,23 @@ class LDaCATabulator:
             df = df.drop(columns=[c for c in to_drop if c in df.columns])
 
         return df
+    
+    def _load_entity_table(self, table_name: str):
+        try:
+            self.tb.entity_table(table_name)
+        except Exception:
+            print(f"No {table_name} table in this corpus.")
+            return None
+
+        df = load_table_from_db(str(self.database), table_name)
+        return drop_id_columns(df)
 
     # get_text: RepositoryObject
     def get_text(self):
-        #TODO some properties may not be expandable for some corpora
         self.tb.entity_table("RepositoryObject")
         df = load_table_from_db(str(self.database), "RepositoryObject")
         df = self._filter_ignored_columns("RepositoryObject", df)
-        df = drop_id_columns(df) 
-        return df
+        return drop_id_columns(df)
 
     # ------------------------------------------------------------
     # get_people()
@@ -72,38 +80,19 @@ class LDaCATabulator:
 
     # TODO it does not remove properties of ids
     def get_people(self):
-
-        try:
-            self.tb.entity_table("Person")
-        except Exception:
-            print("No Person table in this corpus.")
-            return None
-
-        df = load_table_from_db(str(self.database), "Person")
-        return drop_id_columns(df) 
+        return self._load_entity_table("Person")
+ 
 
     # ------------------------------------------------------------
     # get_organization()
     # ------------------------------------------------------------
     def get_organization(self):
-
-        try:
-            self.tb.entity_table("Organization")
-        except Exception:
-            print("No Organization table in this corpus.")
-            return None
-
-        df = load_table_from_db(str(self.database), "Organization")
-        return drop_id_columns(df) 
+        return self._load_entity_table("Organization")
     
-    def get_speaker(self):
- 
-        try:
-            self.tb.entity_table("Speaker")
-        except Exception:
-            print("No Speaker table in this corpus.")
-            return None
 
-        df = load_table_from_db(str(self.database), "Speaker")
-        return drop_id_columns(df) 
+    # ------------------------------------------------------------
+    # get_speaker()
+    # ------------------------------------------------------------
+    def get_speaker(self):
+        return self._load_entity_table("Speaker")
 
